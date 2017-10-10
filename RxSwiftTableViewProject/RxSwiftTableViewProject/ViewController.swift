3ec661d734ad8b5e66f35850da1eab0b7005ac39
Lehlohonolo Mbele <lehlohonoloisaac25@gmail.com>
2017-10-10 13:47:37 +0200

    add methods to handle tapping on cells

--- RxSwiftTableViewProject/RxSwiftTableViewProject/ViewController.swift
+++ RxSwiftTableViewProject/RxSwiftTableViewProject/ViewController.swift
@@ -17,12 +17,45 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        handleModelTapped()
+        handleCellTapped()
     }
     func setupUI(){
         students.bindTo(tableView.rx.items(cellIdentifier: "studentCell", cellType: StudentCell.self)){
             (_, student, cell) in
+                cell.tag = 0
                 cell.textLabel?.text = student.name
         }.addDisposableTo(disposeBag)
+    }
+    
+    func handleModelTapped(){
+        tableView.rx.modelSelected(Student.self).subscribe(onNext: {
+            student in
+            print("\(student.name) has surname : \(student.surname)")
+        })
+        .addDisposableTo(disposeBag)
+    }
+    
+    func handleCellTapped(){
+        tableView.rx.itemSelected.subscribe(onNext : {
+            [weak self] indexPath in
+            if let cell = self?.tableView.cellForRow(at: indexPath) as? StudentCell {
+                self?.updateCell(cell: cell)
+            }
+        })
+        .addDisposableTo(disposeBag)
+    }
+    
+}
+extension ViewController{
+    func updateCell(cell: UITableViewCell){
+        if cell.tag == 0 {
+            cell.backgroundColor = UIColor.blue.withAlphaComponent(0.6)
+            cell.tag = 1
+        }else if cell.tag == 1 {
+            cell.backgroundColor = UIColor.white
+            cell.tag = 0
+        }
     }
 }
 
